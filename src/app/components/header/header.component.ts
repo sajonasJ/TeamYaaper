cb5e9e3c2bb4b2e0adb7741633eb983eb7afc008
sajonasJ <jonas.sajonas@gmail.com>
2024-09-01 12:06:37 +1000

    added header logic and style changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isLoggedIn = false; // Set this dynamically based on actual authentication state
 
+  constructor() {
+    // Check login state from authentication service or local storage
+    // this.isLoggedIn = authService.isLoggedIn();
+  }
+
+  logout() {
+    // Perform logout operation
+    // authService.logout();
+    this.isLoggedIn = false; // Update the state
+  }
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
-export class LoginPageComponent {
-  isSignIn = true;  // By default, show the SignInComponent
+export class LoginPageComponent implements OnInit {
+  isSignIn = true;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      if (params['signup']) {
+        this.switchToSignUp();
+      } else {
+        this.switchToSignIn();
+      }
+    });
+  }
 
   switchToSignUp() {
-    this.isSignIn = false;  // Switch to SignUpComponent
+    this.isSignIn = false;
   }
 
   switchToSignIn() {
-    this.isSignIn = true;  // Switch back to SignInComponent
+    this.isSignIn = true;
   }
 }
