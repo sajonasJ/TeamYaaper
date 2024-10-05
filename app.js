2bf5a69d492fc57a29f3f025b7e1e1be94e2177f
sajonasJ <jonas.sajonas@gmail.com>
2024-10-05 16:01:01 +1000

    added app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// Connection URL
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "teamYaaper";
+
+// Main function that connects to the database
+async function main() {
+  await client.connect();
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  return db;
+}
+
+// Function to close the MongoDB connection
+async function closeConnection() {
+  await client.close();
+  console.log("MongoDB connection closed.");
+}
+
+module.exports = { main, closeConnection };
